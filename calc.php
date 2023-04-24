89af7949728c0b8537dd3c5b48d4fed77ebb6194
PedroFreitazz <130800643+PedroFreitazz@users.noreply.github.com>
2023-04-24 15:39:34 -0300

    Add files via upload

---
+++ calc.php
@@ -0,0 +1,28 @@
+<?php
+if(isset($_POST["btn"])){// querer fazer o php no arq html
+    $n1 = $_POST ["num1"];
+    $n2 = $_POST ["num2"];
+    $op = $_POST ["btn"];
+    switch($op){
+     case '+':
+         $res = $n1 + $n2;
+         break;
+         case '-':
+            $res= $n1 - $n2 ;
+            break;
+            case '*';
+            $res= $n1 * $n2 ;
+            break;
+            case '/';//tratar o n2 ,não poder zerro            
+            if ($n2 != 0)  $res=  $n1 / $n2;
+            else $res= "Não existe divisão por zero";
+            break;
+
+
+
+    }
+echo "<br> <h1>o resultado da op $op é $res </h1>" ;
+echo '<br> <h1>o resultado da op $op é $res </h1>';
+
+}
+?>
